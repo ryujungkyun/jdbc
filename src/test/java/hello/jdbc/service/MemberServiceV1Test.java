cf4d6cb6da86becdb9c138ec7030975f6d3c725c
류정균 <prank01@naver.com>
2025-12-19 01:45:20 +0900

    feat: memberService Test 추가

---
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,29 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.repository.MemberRepositoryV1;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+/**
+ * 기본 동작, 트랜잭션이 없어서 문제 발생
+ */
+class MemberServiceV1Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberA";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+
+    }
+}
